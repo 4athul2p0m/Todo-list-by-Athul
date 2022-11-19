3637a485eb8482cdc533ddf1667d4d9b2acf4d4d
athulpm <97athulpm@gmail.com>
2022-11-19 17:13:25 +0530

    Added filter and filter styling

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var todoInput = document.querySelector('.todo-input');
 var todoButton = document.querySelector('.todo-button');
 var todoList = document.querySelector('.todo-list');
 
-todoButton.innerText = "To do";
 //events
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click', deleteAndCheck);
@@ -22,6 +21,7 @@ function addTodo(e) {
         const newTodo = document.createElement('li');
         newTodo.classList.add('todo-item');
         newTodo.innerText = todoInput.value; //add input text to todo
+        todoInput.value = ""; //clear text field
         todoDiv.appendChild(newTodo);
         //create checked button
         const checkedBtn = document.createElement('button');
